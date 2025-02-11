48075ef39559b05def584fe5e515c368bf1961fa
Jasjeev <jasjeev99@gmail.com>
2025-02-11 14:45:47 +0530

    Add data validation functionality and update requirements for network security project

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=="__main__":
@@ -9,9 +10,17 @@ if __name__=="__main__":
         trainingpipelineconfig = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(data_ingestion_config)
+        
         logging.info("Exporting collection as dataframe")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        
+        logging.info("Exporting data into feature store")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Validating number of columns")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
     except NetworkSecurityException as e:
         logging.error(f"Error in main {e}")
     except Exception as e:
